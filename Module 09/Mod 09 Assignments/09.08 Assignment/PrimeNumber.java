bb85c52b825f0bb2a3554acb866ee6185ee9f8ca
Gyerr <gyerr257@gmail.com>
2023-04-05 17:26:07 -0400

    added documentation

--- Module 09/Mod 09 Assignments/09.08 Assignment/PrimeNumber.java
+++ Module 09/Mod 09 Assignments/09.08 Assignment/PrimeNumber.java
@@ -24,16 +24,21 @@ public class PrimeNumber {
         this.isValid = isValid;
     }
 
+    //main class to loop and display prmes
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        //after scanner created, ask questions for lower and upper limit
         System.out.println("Enter the lower limit for the prime numbers: ");
         int lowerLimit = in.nextInt();
 
         System.out.println("Enter the upper limit for the prime numbers(i.e., 25000): ");
         int upperLimit = in.nextInt();
 
+        //count to display # of primes
         int count = 0;
+
+        //loop to find if numbers are prime in between limits and prints number if it is prime and iterates count by 1 to display # of primes
         for (int number = lowerLimit; number <= upperLimit; number++) {
             if(isPrime(number)) {
                 System.out.println(number);
@@ -44,17 +49,25 @@ public class PrimeNumber {
 
         in.close();
 
+        //display # of primes generated
         System.out.println("Number of primes generated: " + count);
 
 
     }
 
+    //method to calculate if # is prime or not, takes in the number that is being iterated.
     public static boolean isPrime(int number) {
+
+        //i must be 2 because 2 is lowest prime.
         for (int i = 2; i < number; i++) {
+
+            //checks if there is no remainder and if true, isPrime is returned as false as the # is not prime. 
             if (number%i == 0) {
                 return false;
             }
         }
+
+        //returns true if the loop doesn't return as false, acts as an else statement without the else.
         return true;
     }
 }
