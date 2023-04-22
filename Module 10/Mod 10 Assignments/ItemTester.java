03d4ad0051d9888e4641d0feb398bce763d16ef6
Gru Yerramalli <gyerr257@gmail.com>
2023-04-21 20:22:06 -0400

    finished assignment

--- Module 10/Mod 10 Assignments/ItemTester.java
+++ Module 10/Mod 10 Assignments/ItemTester.java
@@ -1,35 +1,47 @@
+/**
+ * @author Gruhith Yerramalli
+ * @version 4/21/23
+ * 
+ * PMR
+ * --------------------------------
+ * (+): Easy to do implementation class and selectino sort and some of insertion sort
+ * 
+ * (-): Merge sort was hard, I had to see reference. I really need to practice this and I am also not good at sorts in general so I need to practice
+ * --------------------------------------
+ * In the Future: Practice WAY MORE SORTS!!!!!!!
+ */
+
 import java.util.Arrays;
 
 public class ItemTester {
     public static void main(String[] args) {
-        Item[] items = new Item[10];
+        Item[] items = new Item[9];
         items[0] = new Item("Chewbacca Mask", 1281, 19.99, 24);
         items[1] = new Item("School Backpack", 2468, 29.99, 10);
         items[2] = new Item("Water Bottle", 1359, 4.99, 50);
         items[3] = new Item("Pencil Case", 3579, 9.99, 15);
         items[4] = new Item("Laptop Sleeve", 4680, 39.99, 5);
-        items[5] = new Item("Stapler", 2579, 7.99, 20);
-        items[6] = new Item("Whiteboard Set", 3579, 14.99, 8);
-        items[7] = new Item("Eraser", 2468, 1.99, 100);
-        items[8] = new Item("Textbook", 4680, 79.99, 3);
-        items[9] = new Item("Calculator", 1357, 49.99, 6);
+        items[5] = new Item("Whiteboard Set", 9650, 14.99, 8);
+        items[6] = new Item("Eraser", 2107, 1.99, 100);
+        items[7] = new Item("Textbook", 4531, 79.99, 3);
+        items[8] = new Item("Calculator", 1357, 49.99, 6);
 
         System.out.println("Original Array:");
         printTable(items);
 
-        sortProductNumber(items, true);
+        sortProductNumber(items, "a");
         System.out.println("\nSorted by Product Number (Ascending):");
         printTable(items);
 
-        sortProductName(items, false);
+        sortProductName(items, "a");
         System.out.println("\nSorted by Product Name (Descending):");
         printTable(items);
 
-        sortPrice(items, true);
+        sortPrice(items, "d");
         System.out.println("\nSorted by Price (Ascending):");
         printTable(items);
 
-        sortQuantity(items, false);
+        sortQuantity(items, 1, 4, 8);
         System.out.println("\nSorted by Quantity (Descending):");
         printTable(items);
     }
@@ -41,35 +53,129 @@ public class ItemTester {
         }
     }
 
-    public static void sortProductNumber(Item[] items, boolean ascending) {
-        if (ascending) {
-            Arrays.sort(items, (a, b) -> a.getProductNumber() - b.getProductNumber());
-        } else {
-            Arrays.sort(items, (a, b) -> b.getProductNumber() - a.getProductNumber());
+    public static void sortProductNumber(Item[] items, String ascending) {
+        int n = items.length;
+        if (ascending.equalsIgnoreCase("a")) {
+            for(int index = 0; index < n-1; index++) {
+                int min_idx = index; // max_idx instead of min_idx
+                    for(int j = index+1; j < n; j++)
+                        if(items[j].getProductNumber() < items[min_idx].getProductNumber()) // > instead of <
+                            min_idx = j;
+
+                Item temp = items[min_idx];
+                items[min_idx] = items[index];
+                items[index] = temp;
+            }
+        } else if (ascending.equalsIgnoreCase("d")){
+            
+            for(int index = 0; index < n-1; index++) {
+                int max_idx = index; // max_idx instead of min_idx
+                    for(int j = index+1; j < n; j++)
+                        if(items[j].getProductNumber() > items[max_idx].getProductNumber()) // > instead of <
+                            max_idx = j;
+
+                Item temp = items[max_idx];
+                items[max_idx] = items[index];
+                items[index] = temp;
+            }
+        }
+    }
+    
+
+    public static void sortProductName(Item[] items, String ascending) {
+        if (ascending.equalsIgnoreCase("a")) {
+            int n = items.length;
+            for (int unsorted = 1; unsorted < n; ++unsorted) {
+                Item nextItem = items[unsorted];
+                int loc = unsorted;
+                while ((loc > 0) &&(items[loc-1].getName().compareTo(nextItem.getName()) > 0)) {
+                  items[loc] = items[loc-1];
+                  loc--;
+                } 
+                items[loc] = nextItem;
+              }
+        } else if (ascending.equalsIgnoreCase("d")) {
+            int n = items.length;
+            for (int unsorted = 1; unsorted < n; ++unsorted) {
+                Item nextItem = items[unsorted];
+                int loc = unsorted;
+                while ((loc > 0) &&(items[loc-1].getName().compareTo(nextItem.getName()) < 0)) {
+                  items[loc] = items[loc-1];
+                  loc--;
+                } 
+                items[loc] = nextItem;
+              }
         }
     }
 
-    public static void sortProductName(Item[] items, boolean ascending) {
-        if (ascending) {
-            Arrays.sort(items, (a, b) -> a.getName().compareToIgnoreCase(b.getName()));
-        } else {
-            Arrays.sort(items, (a, b) -> b.getName().compareToIgnoreCase(a.getName()));
+    public static void sortPrice(Item[] items, String ascending) {
+        int n = items.length;
+        if (ascending.equalsIgnoreCase("a")) {
+            for(int index = 0; index < n-1; index++) {
+                int min_idx = index; // max_idx instead of min_idx
+                    for(int j = index+1; j < n; j++)
+                        if(items[j].getPrice() < items[min_idx].getPrice()) // > instead of <
+                            min_idx = j;
+
+                Item temp = items[min_idx];
+                items[min_idx] = items[index];
+                items[index] = temp;
+            }
+        }        
+         else if (ascending.equalsIgnoreCase("d")) {
+            for(int index = 0; index < n-1; index++) {
+                int max_idx = index; // max_idx instead of min_idx
+                    for(int j = index+1; j < n; j++)
+                        if(items[j].getPrice() > items[max_idx].getPrice()) // > instead of <
+                            max_idx = j;
+
+                Item temp = items[max_idx];
+                items[max_idx] = items[index];
+                items[index] = temp;
+            }
         }
     }
 
-    public static void sortPrice(Item[] items, boolean ascending) {
-        if (ascending) {
-            Arrays.sort(items, (a, b) -> Double.compare(a.getPrice(), b.getPrice()));
-        } else {
-            Arrays.sort(items, (a, b) -> Double.compare(b.getPrice(), a.getPrice()));
+    public static void sortQuantity(Item[] items, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+ 
+        
+        Item L[] = new Item[n1];
+        Item R[] = new Item[n2];
+ 
+        for (int i = 0; i < n1; ++i)
+            L[i] = items[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = items[m + 1 + j];
+ 
+ 
+        int i = 0, j = 0;
+ 
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i].getQuantity() <= R[j].getQuantity()) {
+                items[k] = L[i];
+                i++;
+            }
+            else {
+                items[k] = R[j];
+                j++;
+            }
+            k++;
         }
-    }
-
-    public static void sortQuantity(Item[] items, boolean ascending) {
-        if (ascending) {
-            Arrays.sort(items, (a, b) -> a.getQuantity() - b.getQuantity());
-        } else {
-            Arrays.sort(items, (a, b) -> b.getQuantity() - a.getQuantity());
+ 
+        while (i < n1) {
+            items[k] = L[i];
+            i++;
+            k++;
         }
-    }
+ 
+        while (j < n2) {
+            items[k] = R[j];
+            j++;
+            k++;
+        }
+    } 
 }
+
